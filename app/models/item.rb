fec231a38f2927e9d7009af9772b0043ba877c5d
Sakanakushon <110516513+Sakanakushon@users.noreply.github.com>
2022-12-03 12:51:53 +0000

    顧客、topページ

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
+  
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/写真なし.jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end      
 end
