597af379e8357d3a5a7ffe7b75c2e9e014c3bdfe
Sakanakushon <110516513+Sakanakushon@users.noreply.github.com>
2022-12-04 03:00:24 +0000

    顧客ログイン画面

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -26,7 +26,7 @@ class Public::SessionsController < Devise::SessionsController
   # end
   
   def after_sign_in_path_for(resource)
-    customer_path
+    root_path
   end
   
   def after_sign_out_path_for(resource)
