507c6b92fbc0651867bb09cca93f560c406ff7ec
Sakanakushon <110516513+Sakanakushon@users.noreply.github.com>
2022-12-05 05:47:50 +0000

    顧客情報編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,11 +1,17 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+    redirect_to customers_path(@customer.id)
+    end
   end
 
   def withdrawal_confirmation
@@ -13,4 +19,11 @@ class Public::CustomersController < ApplicationController
 
   def withdrawal_process
   end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted)
+  end
+  
 end
