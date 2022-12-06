ac7f1109f33053f3c780a9bbcc84ec9673f193eb
Sakanakushon <110516513+Sakanakushon@users.noreply.github.com>
2022-12-06 11:49:50 +0000

    配送先一覧、登録、編集

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,37 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.all
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+    redirect_to addresses_path
+    end
   end
 
   def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path(@address)
   end
 
   def destroy
+    @addresses = Address.find(params[:id])
+    @addresses.destroy
+    redirect_to addresses_path
   end
+  
+  private
+  
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address, :customer_id)
+  end
+  
 end
